162d7e2a2682080d3e649e28de138e6c1335e86e
AddyMills <74471839+AddyMills@users.noreply.github.com>
2023-07-08 14:21:55 -0600

    Update compile-folder.bat

--- compile-folder.bat
+++ compile-folder.bat
@@ -1,2 +1,11 @@
-pyinstaller -p "D:\GitHub\Guitar-Hero-III-Tools\create_audio" -p D:\GitHub\Guitar-Hero-III-Tools\midqb_gen -p D:\GitHub\Guitar-Hero-III-Tools\pak_extract -p D:\GitHub\Guitar-Hero-III-Tools\ska_converter -p D:\GitHub\Guitar-Hero-III-Tools -p "D:\GitHub\Guitar-Hero-III-Tools\gui" --add-data "debug.txt;." --add-data "conversion_files_prod;conversion_files" --add-data "create_audio\default_audio\blank.wav;create_audio\default_audio" "GH Toolkit.py"
+pyinstaller -p "D:\GitHub\Guitar-Hero-III-Tools\create_audio" ^
+-p D:\GitHub\Guitar-Hero-III-Tools\midqb_gen ^
+-p D:\GitHub\Guitar-Hero-III-Tools\pak_extract ^
+-p D:\GitHub\Guitar-Hero-III-Tools\ska_converter ^
+-p D:\GitHub\Guitar-Hero-III-Tools ^
+-p "D:\GitHub\Guitar-Hero-III-Tools\gui" ^
+--add-data "debug.txt;." ^
+--add-data "conversion_files_prod;conversion_files" ^
+--add-data "create_audio\default_audio\blank.wav;create_audio\default_audio" ^
+"GH Toolkit.py"
 pause
